fc6e9a9f3c2d6db8bdc25bb738e2bcf3086b01ec
suqinglee <suqinglee@outlook.com>
2022-03-11 15:46:25 +0800

    sgbm

--- calib.py
+++ calib.py
@@ -50,20 +50,59 @@ class Calib3D:
         return K0, D0, K1, D1, R, T
 
     def stereo_rectify(self):
+        # R T 左到右的旋转/投影矩阵，源码注释是 first to second，这里第一个参数就是左相机
         K0, D0, K1, D1, R, T = self.stereo_calibrate()
-        R0, R1, P0, P1, Q, roi0, roi1 = cv2.stereoRectify(K0, D0, K1, D1, self.img_size, R, T, flags = cv2.cv2.CALIB_ZERO_DISPARITY, alpha = 1)
+        # R0 P0 左到右的旋转/投影矩阵，R1 P1 右到左的旋转/投影矩阵
+        R0, R1, P0, P1, Q, roi0, roi1 = cv2.stereoRectify(K0, D0, K1, D1, self.img_size, R, T, flags = cv2.CALIB_ZERO_DISPARITY, alpha = 0)
 
+        # K0 D0 校正失真，R0 P0 校正垂直视差
         m0a, m0b = cv2.initUndistortRectifyMap(K0, D0, R0, P0, self.img_size, cv2.CV_16SC2)
-        img0 = cv2.cvtColor(cv2.remap(self.imgs[0], m0a, m0b, cv2.INTER_LINEAR), cv2.COLOR_GRAY2BGR)
-        m1a, m1b = cv2.initUndistortRectifyMap(K1, D1, R1, P1, self.img_size, cv2.CV_16SC2)
-        img1 = cv2.cvtColor(cv2.remap(self.imgs[self.length], m1a, m1b, cv2.INTER_LINEAR), cv2.COLOR_GRAY2BGR)
+        img0 = cv2.remap(self.imgs[0], m0a, m0b, cv2.INTER_LINEAR)
+        img0c = cv2.cvtColor(img0, cv2.COLOR_GRAY2BGR)
 
-        rectify = np.concatenate((img0, img1), axis = 1)
+        m1a, m1b = cv2.initUndistortRectifyMap(K1, D1, R1, P1, self.img_size, cv2.CV_16SC2)
+        img1 = cv2.remap(self.imgs[self.length], m1a, m1b, cv2.INTER_LINEAR)
+        img1c = cv2.cvtColor(img1, cv2.COLOR_GRAY2BGR)
+
+        rectify = np.concatenate((img0c, img1c), axis = 1)
         rectify[::40, :] = (0, 255, 0)
         cv2.imshow('rectify', rectify)
-        cv2.waitKey(100000)
+
+        return img0, img1, Q
+    
+    def depth_img(self, img0, img1):
+        d = cv2.getTrackbarPos('disparities', 'depth')*16
+        b = cv2.getTrackbarPos('block', 'depth')
+
+        matcher0 = cv2.StereoSGBM_create(0, d, b, 24*b, 96*b, 12, 10, 50, 32, 63, cv2.STEREO_SGBM_MODE_SGBM_3WAY)
+        matcher1 = cv2.ximgproc.createRightMatcher(matcher0)
+        wls_filter = cv2.ximgproc.createDisparityWLSFilter(matcher0)
+        wls_filter.setLambda(80000)
+        wls_filter.setSigmaColor(1.3)
+        disp0 = np.int16(matcher0.compute(img0, img1))
+        disp1 = np.int16(matcher1.compute(img1, img0))
+
+        img = wls_filter.filter(disp0, img0, None, disp1)
+        return np.uint8(cv2.normalize(img, img, 255, 0, cv2.NORM_MINMAX))
+
     
     def Run(self):
-        self.stereo_rectify()
+        img0, img1, Q = self.stereo_rectify()
+
+        def callback(e, x, y, f, p):
+            if e == cv2.EVENT_LBUTTONDOWN:
+                print(coord_3d[y][x])
+        cv2.namedWindow('depth')
+        cv2.createTrackbar('disparities', 'depth', 5, 60, lambda x: None)
+        cv2.createTrackbar('block', 'depth', 3, 17, lambda x: None)
+        cv2.setMouseCallback('depth', callback, None)
+
+        while True:
+            depth = self.depth_img(img0, img1)
+            coord_3d = cv2.reprojectImageTo3D(depth.astype(np.float32)/16., Q)
+            depth = cv2.applyColorMap(depth, cv2.COLORMAP_JET)
+
+            cv2.imshow('depth', depth)
+            cv2.waitKey(100)
 
 Calib3D(os.sep.join([os.path.abspath(os.curdir), 'imgs'])).Run()
